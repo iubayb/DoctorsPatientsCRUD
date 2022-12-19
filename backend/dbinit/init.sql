8892c5b3082ca91199d4dc732e87fdf4077bbeff
iubayb <ayoubrahmanii@gmail.com>
2022-12-19 10:29:12 +0400

    indentation error

--- backend/dbinit/init.sql
+++ backend/dbinit/init.sql
@@ -5,10 +5,11 @@ USE DoctorsPatientsDB;
 CREATE TABLE IF NOT EXISTS Doctors (
       id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
       full_name VARCHAR(255) NOT NULL
-    )
+);
 
 CREATE TABLE IF NOT EXISTS Patients (
       id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
       full_name VARCHAR(255) NOT NULL,
-      doctor_id BIGINT NOT NULL
-    )
+      doctor_id BIGINT UNSIGNED NOT NULL,
+      FOREIGN KEY (doctor_id) REFERENCES Doctors(id)
+);
